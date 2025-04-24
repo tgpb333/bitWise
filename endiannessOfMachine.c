761ed8819886318b0d19dcd88c1c2dc403786313
tgpb333 <prateek.bhatnagar333@gmail.com>
2025-04-24 11:32:20 +0530

    Create endiannessOfMachine.c

---
+++ endiannessOfMachine.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void getEndianness(){
+  unsigned int i = 0x12345678;
+  printf("%x\n", i);
+  unsigned char *c = (char*)&i;
+  printf("%x\n", *c);
+  c++;
+  printf("%x\n", *c);
+  c+=2;
+  printf("%x\n", *c);
+  if(*c)
+    printf("Little Endian : \n");
+  else
+    printf("Big Endian : %c\n");
+}
+
+int main(){
+  getEndianness();
+  return 0;
+}
